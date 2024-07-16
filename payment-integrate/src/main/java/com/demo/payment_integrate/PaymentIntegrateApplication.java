bb5ba1cefaff5603441c5b20e411f33b96f738b6
Pratik <pratikjha.544@gmail.com>
2024-07-16 07:03:13 -0700

    evening

---
+++ payment-integrate/src/main/java/com/demo/payment_integrate/PaymentIntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.demo.payment_integrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrateApplication.class, args);
+	}
+
+}
---
+++ payment-integrate/src/test/java/com/demo/payment_integrate/PaymentIntegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.payment_integrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
